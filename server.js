78f2f50d663753b60b7381b910cba7044325dfa1
alk3mik <mozz76@gmail.com>
2017-05-24 21:07:55 +0200

    Started to create the 4+1 CRUD routes

--- server.js
+++ server.js
@@ -1,3 +1,30 @@
+const express = require('express');
+const app = express();
+
+// app listening on port 9000 (the server functions on port 9000)
+app.listen(9000);
+
+// To serve static files such as images, CSS files, and JavaScript
+// files, contained in the directory 'public', use the express.static
+// built-in middleware function in Express.
+app.use(express.static('public'));
+
+// We will create 4+1 CRUD (create, read, update, and delete) routes.
+//
+// Create (POST) - Make something
+// Read (GET)_- Get something
+// Update (PUT) - Change something
+// Delete (DELETE)- Remove something
+//
+// app.get(path, callback)
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+  // Note: __dirname is directory that contains the JavaScript source code,
+  // that is: /Users/max/Work/JavaScript/WildCodeSchool/Ateliers/NodeJS/atelier_node_express
+});
+
+
+
 /*
 
     // À utiliser:
