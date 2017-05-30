25c74460d31d0e765c07ed84aed35fc293e82b35
alk3mik <mozz76@gmail.com>
2017-05-30 02:52:09 +0200

    many things were understood ..\! Seriously\!

--- server.js
+++ server.js
@@ -11,10 +11,12 @@ var urlParser = bodyParser.urlencoded({ extended: false });
 // To serve static files such as images, CSS files, and JavaScript
 // files, contained in the directory 'public', use the express.static
 // built-in middleware function in Express.
+//
 app.use(express.static('public'));
 
 // Enable CORS (Cross-Origin Resource Sharing) header on ExpressJS
 // more info in https://www.html5rocks.com/en/tutorials/cors/
+//
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -22,9 +24,9 @@ app.use(function(req, res, next) {
 });
 
 // to support JSON-encoded bodies
-// app.use(jsonParser);
+app.use(jsonParser);
 // to support URL-encoded bodies
-// app.use(urlParser);
+app.use(urlParser);
 
 // We will create 4+1 CRUD (create, read, update, and delete) routes.
 //
@@ -33,11 +35,16 @@ app.use(function(req, res, next) {
 // Update (PUT) - Change something
 // Delete (DELETE)- Remove something
 
+var db = require('./mocks/ninjas.json');
+// console.log(db, typeof db);
+
 // GET (R.ead) all the ninjas JSON file
-app.get('/mocks/ninjas', jsonParser, function(req, res, next) {
+app.get('/ninja',  function(req, res) {
+	// res.json({ message: 'hooray! welcome to our api!' });
+	res.send(db);
 	// var test = require('url').parse(req.url).query;
 	// console.log(test);
-	console.log(req.body, req.query);
+	// console.log(req.body, req.query);
 	// console.log(req.query, req.body);
 	// res.send(req.query);
 
@@ -47,26 +54,46 @@ app.get('/mocks/ninjas', jsonParser, function(req, res, next) {
 });
 
 //POST (C.reate)
-// app.post('/mocks', function (req, res, next) {
-	// var myNinjaObj = {
-	// 	_id: "666b66666f666666a6666c66",
-	// 	age: 99,
- //        eyeColor: "red",
- //        name: {
- //                first: "Thurston",
- //                last: "Moore"
- //              },
- //        gender: "unknown",
- //        clan: "SONIC YOUTH"
-	// };
+app.post('/ninja', function (req, res) {
+// We send the ninja to be created (added) in POSTMAN 
+// as *raw* data and set the type to *application/json*
+/*
+	{
+		"_id": "666b66666f666666a6666c66",
+		"age": 99,
+		"eyeColor": "red",
+		"name": {
+			"first": "Thurston",
+			"last": "Moore"
+		},
+		"gender": "unknown",
+		"clan": "SONIC YOUTH"
+	}
+*/
 
-	// req.body = myNinjaObj;
-    
-// 	// res.status(200).send("post! i.e. create!");
-	// console.log(req.body);
-	// res.send(req.body);
-//       // res.json({message : "Ajoute une nouvelle piscine à la liste", methode : req.method});
-// });
+	// res.json(req.body);
+	
+	var newNinja = createNinja(req.body);
+	console.log(newNinja);
+});
+
+function createNinja(reqBody) {
+
+	var ninjaCreated = {
+		_id: reqBody._id,
+		age: reqBody.age,
+		eyeColor: reqBody.eyeColor,
+		name: {
+				first: reqBody.name.first,
+				last: reqBody.name.last
+			},
+		gender: reqBody.gender,
+		clan: reqBody.clan
+	}
+
+	return ninjaCreated;
+
+}
 
 // // GET only one ninja from the JSON file
 //
@@ -82,24 +109,46 @@ app.get('/mocks/ninjas', jsonParser, function(req, res, next) {
 // Request URL: http://localhost:3000/users/34/books/8989
 // req.params: { "userId": "34", "bookId": "8989" } .
 //
-// app.get('/mocks/ninjas:_id', function(req, res, next) {
-// 	console.log(req.params);
-// 	console.log(req.query.id);
-// 	res.send(req.query);
-// 	// var profile_id = req.query.id;
-//  //    res.render('mocks', {id:profile_id});
-// });
+app.get('/ninja/:id', function(req, res) {
 
-// //PUT (U.pdate)
-// app.put('/mocks', function (req, res) {
-// 	res.status(200).send("put! i.e. update!");
-//       // res.json({message : "Mise à jour des informations d'une piscine dans la liste", methode : req.method});
-// });
-// //DELETE (D.elete)
-// app.delete('/mocks', function (req, res) {
-// 	res.status(200).send("delete! i.e. delete!");
-// 	  // res.json({message : "Suppression d'une piscine dans la liste", methode : req.method});  
-// });
+	// for (var prop in db) {
+	// 	// ...do something...
+	// }
+
+	var gotNinja = findNinja(req.params.id);
+	res.json(gotNinja);
+
+});
+
+function findNinja(reqParamsId) {
+
+	var len = db.ninjas.length;
+	var i = 0;
+
+	while (i < len) {
+		if (db.ninjas[i]._id === reqParamsId) {
+			return db.ninjas[i];
+			break;
+		}
+		i++;
+	}
+
+}
+
+//PUT (U.pdate)
+app.put('/mocks', function (req, res) {
+	res.status(200).send("put! i.e. update!");
+      // res.json({message : "Mise à jour des informations d'une piscine dans la liste", methode : req.method});
+});
+//DELETE (D.elete)
+app.delete('/mocks', function (req, res) {
+	res.status(200).send("delete! i.e. delete!");
+	  // res.json({message : "Suppression d'une piscine dans la liste", methode : req.method});  
+});
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /ninjas
+// app.use('/ninjas', express.Router());
 
 // app listening on port 9000 (the server functions on port 9000)
 app.listen(9000);
