619f269f1a6460649e881e6e337286a2733ed3d2
alk3mik <mozz76@gmail.com>
2017-07-01 20:10:09 +0200

    Important steps ahead have been done. The service  was finally understood how it works. I was able to re-display the list if ninjas in the homepage thanks to the  implementation

--- public/app/components/home/home.js
+++ public/app/components/home/home.js
@@ -3,17 +3,66 @@
 angular.module("ninjaApp")
 
     .component("home", {
-        templateUrl: '/app/components/home/home.html',
-        controller: function ($scope, Ninja) {
+            templateUrl: '/app/components/home/home.html',
+            // controller: function ($scope, $http) {
+/*
+// With the "standard" method (i.e. $http AJAX request), by having injected the dependency $http into
+// the controller, we would do:
+
+            let ninjas_data = $http.get('/ninja');
+
+// And then we would have 2 choices: ECMAScript 3/5 syntax or ECMAScript 6 syntax.
+
+// ECMAScript 3/5 (choice 1).
+// Until arrow functions, every new function defined its own *this* value (a new object in the case of a
+// constructor, undefined in strict mode function calls, the base object if the function is called as an
+// "object method", etc.). This proved to be annoying with an object-oriented style of programming.
+// In ECMAScript 3/5, the *this* issue was fixable by assigning the value in *this* to a variable (named *that*)
+// that could be closed over:
+
+            // let that = this;
+            // ninjas_data.then(function(result) {
+            //       that.ninjas = result.data.ninjas;
+            // });
+
+// ECMAScript 6 (choice 2).
+// Note: An arrow function does not create its own *this*, the *this* value of the enclosing execution
+// context is used. Thus, in the following code, *this* within function passed to .then method has the
+// same value as *this* in the enclosing function (i.e. the value of the *controller* property):
+
+            ninjas_data.then((result) => {
+                  this.ninjas = result.data.ninjas;
+            });
+*/
+
+            controller: function ($scope, Ninja) {
+// By, instead, using the $resource named *Ninja*, we will have:
+//
+// ECMAScript 3/5 (choice 1)
+            // let that = this;
+            // Ninja.query(function(result) {
+            //       // console.log(result, result.ninjas);
+            //       that.ninjas = result.ninjas;
+            //       // console.log(that.ninjas);
+            // });
+
+// ECMAScript 6 (choice 2)
+            Ninja.query((result) => {
+                  this.ninjas = result.ninjas;
+            });
 
             // var ninja = Ninja.get({ id: $scope.id }, function() {
-            //     console.log(ninja, typeof ninjas);
+            //     console.log(ninja, typeof ninja);
             // }); // get() returns a single ninja
-
-            var ninjas = Ninja.query(function() {
-                // console.log(ninjas, typeof ninjas);
-            }); // query() returns all the ninjas
-            $scope.ninjas = ninjas;
+// const that = this;
+            // $resource("/ninja").get().$promise.then(function(Bdd){
+            //       console.log(Bdd)
+            //       that.ninjas = Bdd.ninjas
+            // })
+            // var ninjas = Ninja.get(function() {
+// that.ninjas = ninjas;
+            // }); // query() returns all the ninjas
+            // console.log(ninjas.ninjas, typeof ninjas);
 
             // $scope.ninja = new Ninja(); //You can instantiate resource class
             // // $scope.ninja.data = 'some data';
--- public/app/resources.js
+++ public/app/resources.js
@@ -2,37 +2,34 @@
 
 angular.module("ninjaApp")
 
-    // déclaration de la factory "MaRessource"
-    // .factory("MaRessource", ["$resource", function($resource) {
-    .factory("Ninja", ["$resource", function($resource) {
-        // return $resource("vers où ?", {"des-param": "jenesaispas?"})
-        return $resource("/ninja/:id", {id: "@_id"}, {
-        	
-        	// 'get': {
-        	// 	method: 'GET'
-        	// },
-        	
-        	// 'save': {
-        	// 	method: 'POST'
-        	// },
-        	
-        	// 'remove': {
-        	// 	method: 'DELETE'
-        	// },
+// déclaration de la factory "MaRessource"
+// .factory("MaRessource", ["$resource", function($resource) {
+.factory("Ninja", ["$resource", function($resource) {
+    let url = "http://localhost:9000/ninja";
+    // return $resource("vers où ?", {"des-param": "jenesaispas?"})
+    return $resource(url + "/:id", {id: "@_id"}, {
+        
+        // 'save': {
+        //  method: 'POST'
+        // },
+        
+        // 'remove': {
+        //  method: 'DELETE'
+        // },
+        
+        'query': {
+            method: 'GET',
+            isArray: false
+            // params: {},
+            // cache: true,
+            // transformResponse,
+            // interceptor
+        },
 
-        	// 'delete': {
-        	// 	method:'DELETE'
-        	// },
-        	
-        	'query': {
-        		method: "GET",
-        		isArray: false
-        	},
+        'update': {
+            method: 'PUT' // this method issues a PUT request
+        }
 
-        	'update': {
-        		method: 'PUT' // this method issues a PUT request
-        	}
-
-        });
-    }]);
-    // Maintenant MaRessource est injectable dans un controller ou un autre service
+    });
+}]);
+// Maintenant MaRessource est injectable dans un controller ou un autre service
